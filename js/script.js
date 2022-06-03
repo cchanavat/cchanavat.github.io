82f6aa025db04c86ef1a60658e541bb2c477adfd
mchana <mathieu.chanavat@gmail.com>
2022-06-03 23:13:25 +0200

    js + typos

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+var dictContentElements;
+var dictNavElements;
+
+
+window.onload = function() {
+    dictContentElements = {
+        "about": document.getElementById("content_about"),
+        "work": document.getElementById("content_work"),
+        "contact": document.getElementById("content_contact"),
+    };
+    dictNavElements = {
+        "about": document.getElementById("nav_about"),
+        "work": document.getElementById("nav_work"),
+        "contact": document.getElementById("nav_contact"),
+    }
+}
+
+function hideAllContent() {
+    for (const [_, v] of Object.entries(dictContentElements)) {
+        if (!v.classList.contains("hidden")) {
+            v.classList.add("hidden")
+        }
+    }
+}
+
+function deactivateAllNAv() {
+    for (const [_, v] of Object.entries(dictNavElements)) {
+        if (v.classList.contains("navigation__link--is-active")) {
+            v.classList.remove("navigation__link--is-active")
+        }
+    }
+}
+function toggleContent(id) {
+    console.log(id);
+    var contentElement = dictContentElements[id];
+    var navElement = dictNavElements[id];
+    
+    hideAllContent();
+    deactivateAllNAv();
+
+    contentElement.classList.remove("hidden"); 
+    navElement.classList.add("navigation__link--is-active");
+}
